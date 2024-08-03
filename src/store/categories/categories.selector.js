380ddc3017c3233c5f82c00b653dc3dee70396b8
Denys <deibukdenyscloud@gmail.com>
2024-08-03 20:57:16 +0300

    Add memoization to the category selector using reselect

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,6 +1,23 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+const selectCategorySlice = (state) => {
+  return state.categories;
+};
+
+export const selectCategories = createSelector(
+  [selectCategorySlice],
+  (categorySlice) => {
+    return categorySlice.categories;
+  },
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categoriesArray) => {
+    return categoriesArray.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  },
+);
