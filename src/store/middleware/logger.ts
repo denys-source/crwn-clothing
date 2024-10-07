e73055f6529ceef76a4fcc0676ecc34b3fa3116c
Denys <deibukdenyscloud@gmail.com>
2024-10-07 23:52:52 +0300

    Add typing annotations for the custom middleware

---
+++ src/store/middleware/logger.ts
@@ -0,0 +1,23 @@
+import { Middleware, UnknownAction } from "redux";
+
+import { RootState } from "../store";
+
+export const loggerMiddleware: Middleware<{}, RootState> = (store) => (next) => (action) => {
+  if (isUnknownAction(action)) {
+    if (!action.type) {
+      next(action);
+    }
+
+    console.log("type: ", action.type);
+    console.log("payload: ", action.payload);
+    console.log("current state: ", store.getState());
+
+    next(action);
+
+    console.log("next state: ", store.getState());
+  }
+};
+
+function isUnknownAction(action: unknown): action is UnknownAction {
+  return typeof action === "object" && action !== null && "type" in action;
+}
